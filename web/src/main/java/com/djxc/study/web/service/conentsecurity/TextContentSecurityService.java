a399925f50cfd64244ae50109fc9003f844b3ed2
djxc <dj_jxc@163.com>
2022-12-09 09:42:17 +0800

    去除敏感信息

--- web/src/main/java/com/djxc/study/web/service/conentsecurity/TextContentSecurityService.java
+++ web/src/main/java/com/djxc/study/web/service/conentsecurity/TextContentSecurityService.java
@@ -14,8 +14,8 @@ public class TextContentSecurityService {
         try {
             // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
             // 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
-            Credential cred = new Credential("AKIDuhJl072iCUaYMwMfpqfJoAZo1jJ2MWTA",
-                    "AByXt066uYAdyb1EtVxBLMAkAPQCj9NO");
+            Credential cred = new Credential("xxx",
+                    "xxx");
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
             HttpProfile httpProfile = new HttpProfile();
             httpProfile.setEndpoint("tms.tencentcloudapi.com");
